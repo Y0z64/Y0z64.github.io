5de960290271ccc760dac8c593011a8185248a36
Y0z64 <yairprogrammer@gmail.com>
2024-05-01 00:17:11 -0600

    Small correction

--- src/components/atoms/TechCell.tsx
+++ src/components/atoms/TechCell.tsx
@@ -5,5 +5,6 @@ type Props = {
 
 export default function TechCell({ name }: Props) {
   return <div className="w-full aspect-square rounded-[2.7rem] bg-white flex justify-center items-center p-4" >
+    <img src="" alt="" className=""/>
   </div>;
 }
