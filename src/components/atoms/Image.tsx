508a2e3a92e70b1f2ab1768771e79b55feadaeac
Y0z64 <yairprogrammer@gmail.com>
2024-08-06 18:05:44 -0600

    Added dialog to image

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { PropsWithChildren, useState } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "./Dialog";
 
 interface Props extends PropsWithChildren {
   idx: number;
@@ -7,24 +8,34 @@ interface Props extends PropsWithChildren {
   alt: string | undefined;
 }
 
-export default function Image({idx, image, alt}: Props) {
+export default function Image({ idx, image, alt }: Props) {
   const [show, setShow] = useState(false);
-  
+
   const handleClick = () => {
-    setShow(!show)
-  }
+    setShow(!show);
+  };
 
   return (
-    <motion.li
-      key={idx}
-      className="rounded-xs mx-2 flex aspect-video h-full min-h-[15.5rem] w-fit items-center justify-center px-2 md:max-h-[26rem] md:min-h-[22.5rem] lg:max-h-[40rem] lg:min-h-[34rem] lg:rounded-md lg:first-of-type:ml-4 lg:last-of-type:mr-4 dark:invert-0"
-    >
-      <img src={image} alt={alt} className="pointer-events-none aspect-video h-min w-full" onClick={() => handleClick()}/>
-      {show && (
-        <div className="absolute left-0 top-0 h-screen w-screen bg-black bg-opacity-70 flex items-center justify-center pointer-events-none" >
-          
-        </div>
-      )}
-    </motion.li>
+    <Dialog>
+      <DialogTrigger>
+        <motion.li
+          key={idx}
+          className="rounded-xs mx-2 flex aspect-video h-full min-h-[15.5rem] w-fit items-center justify-center px-2 md:max-h-[26rem] md:min-h-[22.5rem] lg:max-h-[40rem] lg:min-h-[34rem] lg:rounded-md lg:first-of-type:ml-4 lg:last-of-type:mr-4 dark:invert-0"
+        >
+          <img
+            src={image}
+            alt={alt}
+            className="pointer-events-none aspect-video h-min w-full"
+            onClick={() => handleClick()}
+          />
+          {show && (
+            <div className="pointer-events-none absolute left-0 top-0 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70"></div>
+          )}
+        </motion.li>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>Hello world</DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
